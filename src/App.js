ec8b155e3ee07d44888b67136bb128f3a574947b
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-07-21 23:18:56 +0530

    added winning logic

--- src/App.js
+++ src/App.js
@@ -19,15 +19,63 @@ const App = ()=>{
         itemArray.fill("empty",0,9);
     }
 
-    const checkIsWinner=()=>{
-        
-    }
-
+    const checkIsWinner = () => {
+        //  checking  winner of the game
+        if (
+          itemArray[0] === itemArray[1] &&
+          itemArray[0] === itemArray[2] &&
+          itemArray[0] !== "empty"
+        ) {
+          setWinMessage(`${itemArray[0]} won`);
+        } else if (
+          itemArray[3] !== "empty" &&
+          itemArray[3] === itemArray[4] &&
+          itemArray[4] === itemArray[5]
+        ) {
+          setWinMessage(`${itemArray[3]} won`);
+        } else if (
+          itemArray[6] !== "empty" &&
+          itemArray[6] === itemArray[7] &&
+          itemArray[7] === itemArray[8]
+        ) {
+          setWinMessage(`${itemArray[6]} won`);
+        } else if (
+          itemArray[0] !== "empty" &&
+          itemArray[0] === itemArray[3] &&
+          itemArray[3] === itemArray[6]
+        ) {
+          setWinMessage(`${itemArray[0]} won`);
+        } else if (
+          itemArray[1] !== "empty" &&
+          itemArray[1] === itemArray[4] &&
+          itemArray[4] === itemArray[7]
+        ) {
+          setWinMessage(`${itemArray[1]} won`);
+        } else if (
+          itemArray[2] !== "empty" &&
+          itemArray[2] === itemArray[5] &&
+          itemArray[5] === itemArray[8]
+        ) {
+          setWinMessage(`${itemArray[2]} won`);
+        } else if (
+          itemArray[0] !== "empty" &&
+          itemArray[0] === itemArray[4] &&
+          itemArray[4] === itemArray[8]
+        ) {
+          setWinMessage(`${itemArray[0]} won`);
+        } else if (
+          itemArray[2] !== "empty" &&
+          itemArray[2] === itemArray[4] &&
+          itemArray[4] === itemArray[6]
+        ) {
+          setWinMessage(`${itemArray[2]} won`);
+        }
+      };
     const changeItem = itemNumber =>{
         if(winMessage)
             return toast(winMessage,{type:"success"})
 
-        if(itemArray[itemNumber]){
+        if(itemArray[itemNumber]==="empty"){
             itemArray[itemNumber] = isCross ? "cross" : "circle"
             setIsCross(!isCross)
         }else{
@@ -61,7 +109,7 @@ const App = ()=>{
                     <div className="grid">
                         {
                             itemArray.map((item,index)=>{
-                                return <Card>
+                                return <Card onClick={()=>changeItem(index)}>
                                     <CardBody className="box">
                                         <Icon name={item} />
                                     </CardBody>
