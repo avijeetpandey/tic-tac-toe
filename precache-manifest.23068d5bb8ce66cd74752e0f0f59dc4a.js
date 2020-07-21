c213f920d19f2852cb4a7428d71d4b032a35978d
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-07-21 23:27:09 +0530

    Updates

---
+++ precache-manifest.23068d5bb8ce66cd74752e0f0f59dc4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5292d6175c79afda9f774948e626cf5f",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "19def0ba736641915ed1",
+    "url": "/tictac/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "24a1ab11bae6b28d9618",
+    "url": "/tictac/static/css/main.b4060940.chunk.css"
+  },
+  {
+    "revision": "19def0ba736641915ed1",
+    "url": "/tictac/static/js/2.9a2d68bd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tictac/static/js/2.9a2d68bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a1ab11bae6b28d9618",
+    "url": "/tictac/static/js/main.6d8cb41f.chunk.js"
+  },
+  {
+    "revision": "5fc4ae24f7a0a810f2d8",
+    "url": "/tictac/static/js/runtime-main.da548f1f.js"
+  }
+]);
