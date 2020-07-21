c92f8757e642ed00325af9f4821bc47002f5e29d
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-07-21 21:04:31 +0530

    added icons component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react"; 
+import ReactDOM from "react-dom";
+import App from './App';
 
-const App = ()=>{
-    return <h1>Hello World</h1>
-}
-
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
