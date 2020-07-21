2ab2da04843572e9e0f6188cee8feb3048fb2c45
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-07-21 23:05:05 +0530

    completed layoout 🐱‍🏍✔

--- src/App.js
+++ src/App.js
@@ -13,15 +13,28 @@ const App = ()=>{
     const [winMessage,setWinMessage] = useState("");
 
     const reloadGame=()=>{
-
+        //reloads the game
+        setIsCross(false);
+        setWinMessage("");
+        itemArray.fill("empty",0,9);
     }
 
     const checkIsWinner=()=>{
-
+        
     }
 
     const changeItem = itemNumber =>{
+        if(winMessage)
+            return toast(winMessage,{type:"success"})
 
+        if(itemArray[itemNumber]){
+            itemArray[itemNumber] = isCross ? "cross" : "circle"
+            setIsCross(!isCross)
+        }else{
+            return toast("already filled" , {type : "error"})
+        }
+
+        checkIsWinner();
     }
 
     return(
@@ -29,6 +42,22 @@ const App = ()=>{
             <ToastContainer position="bottom-center" />
             <Row>
                 <Col md={6} className="offset-md-3">
+                    {winMessage ? (
+                        <div className="mb-2 mt-2">
+                            <h1 className="text-primary text-center text-uppercase">
+                                {winMessage}
+                            </h1>
+                            <Button 
+                            color="success"
+                            block
+                            onClick={reloadGame}
+                            >Relaod Game</Button>
+                        </div>
+                    ) : (
+                        <h1 className="text-center text-warning">
+                            {isCross ? "cross" : "circle"} turns
+                        </h1>
+                    )} 
                     <div className="grid">
                         {
                             itemArray.map((item,index)=>{
